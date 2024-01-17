2299bbe138992642c40ba733ec9554a19af9a6e1
rakibul Islam <irakibul026@gmail.com>
2024-01-17 10:10:51 +0530

    .

--- src/db/index.js
+++ src/db/index.js
@@ -8,7 +8,6 @@ const connectDB = async () => {
 		console.log(`MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
 	} catch (error) {
 		console.log("MONGODB connection FAILED ", error);
-
 		process.exit(1);
 	}
 };
